6018933d1cc1c3336f8c32312b97d75a165832ce
Fernando Selvino de Sousa <fernandoselvino@gmail.com>
2018-12-17 19:07:50 -0200

    Acessando modulos

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -1,2 +1,13 @@
 const modulaA = require('../../moduloA')
+const saudacao = require('saudacao')
+const c = require('./pastaC')
 console.log(modulaA.ola)
+console.log(saudacao.ola)
+
+console.log(c.ola2)
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom dia')
+//     res.end()
+// }).listen(8080)
